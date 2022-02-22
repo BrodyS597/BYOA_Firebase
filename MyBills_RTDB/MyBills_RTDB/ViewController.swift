13072e36a8cecf1ccb280d9921126f45985b9d82
Brody Sears <brodysears@Brodys-Mac-mini.local>
2022-02-22 13:56:46 -0700

    Initial commit

---
+++ MyBills_RTDB/MyBills_RTDB/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyBills_RTDB
+//
+//  Created by Brody Sears on 2/22/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
